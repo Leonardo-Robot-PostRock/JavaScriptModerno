686d755a8f1812256a4c4ccfa72474330dbf0586
Leonardo <leonardo38361@gmail.com>
2023-07-12 21:55:57 -0300

    comentarios

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,4 +1,5 @@
 const nombre = prompt("¿Cuál es tu nombre?");
+// Toma el nombre y lo muestra por pantalla
 document.querySelector(
   ".contenido"
 ).innerHTML = `${nombre} está aprendiendo javascript Moderno`;
