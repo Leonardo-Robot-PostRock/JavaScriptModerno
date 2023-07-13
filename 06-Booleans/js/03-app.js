166a22ca5ee78e3404d32107e66abd0eb3dfeea5
Leonardo <leonardo38361@gmail.com>
2023-07-13 17:01:34 -0300

    Buenas prácticas al momento de evaluar booleans

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,9 @@
+const autenticado = false;
+
+if (autenticado) {
+  console.log("Si puedes ver netflix");
+} else {
+  console.log("No, no puedes ver netflix");
+}
+
+console.log(autenticado ? "Si esta autenticado" : "No, no esta autenticado");
