5805693bfdf493ca78ffa16675f6cb4091ae37fc
Leonardo <leonardo38361@gmail.com>
2023-07-13 02:05:33 -0300

    variables string escapar comillas con barra \ y metodos string

--- 03-Strings/js/01-app.js
+++ 03-Strings/js/01-app.js
@@ -1,5 +1,5 @@
-const producto = "Monitor 20 Pulgadas";
-const producto2 = String("Monitor 24 Pulgadas");
+const producto = 'Monitor 20"';
+const producto2 = String('Monitor 24 "');
 
 const producto3 = new String("Monitor 27 Pulgadas");
 
---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,9 @@
+const producto = "Monitor 20 Pulgadas";
+
+console.log(producto);
+console.log(producto.length);
+
+console.log(producto.indexOf("Pulgadas"));
+console.log(producto.includes("Tablet"));
+console.log(producto.includes("monitor"));
+console.log(producto.includes("Monitor"));
