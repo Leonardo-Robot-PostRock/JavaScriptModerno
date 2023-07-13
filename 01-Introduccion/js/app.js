c9d8bd5f6d39de9bfb3e809647ef219bd4ce3b47
Leonardo <leonardo38361@gmail.com>
2023-07-13 01:21:49 -0300

    rename app.js

--- 01-Introduccion/js/app.js
+++
@@ -1,5 +0,0 @@
-const nombre = prompt("¿Cuál es tu nombre?");
-// Toma el nombre y lo muestra por pantalla
-document.querySelector(
-  ".contenido"
-).innerHTML = `${nombre} está aprendiendo javascript Moderno`;
