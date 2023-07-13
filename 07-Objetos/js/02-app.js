318d6301890e710df3586e79186ba91759ff03d4
Leonardo <leonardo38361@gmail.com>
2023-07-13 20:07:02 -0300

    acceder a valores, eliminar valores de un objeto

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,20 @@
+const producto = {
+  nombre: "Monitor 20 Pulgadas",
+  precio: 300,
+  disponible: true,
+};
+
+console.log(producto);
+
+// console.log(producto.nombre);
+// console.log(producto.precio);
+// console.log(producto.disponible);
+
+// Agregar nuevas propiedades al objeto
+producto.imagen = "imagen.jpg";
+
+// Eliminar propiedades del objeto
+delete producto.disponible;
+
+console.log(producto);
+// console.log(producto["nombre"]);
