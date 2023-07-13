59b81dbcd42f48affbb3f18e8288c4dc76c897ce
Leonardo <leonardo38361@gmail.com>
2023-07-13 01:41:12 -0300

    arreglos de archivos

--- 01-Introduccion/app.js
+++ 01-Introduccion/app.js
@@ -1,4 +1,3 @@
-
 // VS Cdoe
 
 //     <!-- 02 - JavaScript en el HTML
@@ -11,23 +10,17 @@
 //     alert("Hola Mundo")
 // </script> -->
 
-
-
-
-// <!-- 03 - JavaScript Externo 
-//     Usualmente tu código JavaScript se recomienda colocarlo en un archivo externo 
+// <!-- 03 - JavaScript Externo
+//     Usualmente tu código JavaScript se recomienda colocarlo en un archivo externo
 //     veamosc como hacerlo
 // -->
 
-
 // alert("Hola Mundo")
-'use strict';
-
+"use strict";
 
 // 04 - Veamos como crear nuestro primer programa en JS
 // const nombre = prompt('Cual es tu nombre?');
 // document.querySelector('.contenido').innerHTML = ` ${nombre} está aprendiendo JavaScript Moderno`;
-
 
 // 05 - Comentar el código
 
@@ -40,7 +33,6 @@
 /*
 
 */
-+
 
 // 06 - Todos los navegadores tienen una consola de Javascript, las mejores sin duda son firefox y Chrome
 // La consola es muy útil ya que podrás observar tu código JS, resultados y seleccionar elementos
@@ -53,8 +45,8 @@ console.log(hola);
 console.log([1, 2, 3, 4]);
 
 // De la misma forma se pueden crear objetos
-let obj = { nombre: "juan", profesion: "Desarrollador Web" }
-console.log( obj );
+let obj = { nombre: "juan", profesion: "Desarrollador Web" };
+console.log(obj);
 
 // También se pueden imprimir resultados como tabla
 console.table([1, 2, 3, 4]);
@@ -69,21 +61,23 @@ console.clear();
 console.warn("Eso no esta permitido");
 
 // También monitorear el tiempo que tarda algo en ejecutarse
-console.time('Hola'); console.warn("Eso no esta permitido"); console.warn("Eso no esta permitido");
+console.time("Hola");
 console.warn("Eso no esta permitido");
-console.timeEnd('Hola');
-   
+console.warn("Eso no esta permitido");
+console.warn("Eso no esta permitido");
+console.timeEnd("Hola");
 
 // 07 - Muchas programadores cometen el error de que se fijan en tener un código muy ordenado en lugar de preocuparse por aprender cosas más avanzadas, otros cometen el error al reves, aprenden un lenguaje a profunidad pero su código es muy desordenado y dificil de leer
 
 // JavaScript tiene una caracteristica, a diferencia de otros lenguajes de programación el ; al final  no es obligatorio salvo que tengas 2 líneas de código en una misma.
 // ejemplo
 
-console.log('Hola')
-console.log('Mundo')
+console.log("Hola");
+console.log("Mundo");
 
-// va a funcionar, pero si tienes un código desordenado; no va a funcionar salvo que pongas ; 
-console.log('Hola'); console.log('Mundo');
+// va a funcionar, pero si tienes un código desordenado; no va a funcionar salvo que pongas ;
+console.log("Hola");
+console.log("Mundo");
 
 // Como recomendación siempre pon una instrucción por linea, no es obligatorio salvo el caso que vimos anteriormente pero va a facilitar mucho tu código
 
@@ -93,7 +87,7 @@ console.log('Hola'); console.log('Mundo');
 // console.log('ok')
 // }
 
-// en estos casos esta función es valida y va a funcionar, pero es mejor utilizar tabs o espacios 
+// en estos casos esta función es valida y va a funcionar, pero es mejor utilizar tabs o espacios
 
 // 08 - Obteniendo ayuda con ESLint para revisar errores de código.
 
---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,14 @@
+let producto = "Tablet";
+
+// Reasignar el valor
+producto = "Monitor";
+producto = true;
+producto = null;
+
+console.log(producto);
+
+let precio;
+
+precio = 300;
+
+console.log(precio);
