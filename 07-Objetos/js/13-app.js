2fd7c11ec62a94a569ff5ff8ae1fe8fda6090529
Leonardo <leonardo38361@gmail.com>
2023-07-14 17:55:40 -0300

    methods:Object keys,values,entries

---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,14 @@
+const producto = {
+  nombre: "Monitor 20 Pulgadas",
+  precio: 300,
+  disponible: true,
+};
+
+//Obtener las llaves del objeto
+console.log(Object.keys(producto));
+
+//Obtener el valor de las llaves o posiciones del objeto
+console.log(Object.values(producto));
+
+//Obtener llaves y valores del objeto
+console.log(Object.entries(producto));
