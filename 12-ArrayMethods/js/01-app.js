1f2429926b421005aed15c48fc776342c1f7a68a
Leonardo <leonardo38361@gmail.com>
2023-07-17 19:30:06 -0300

    some method

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -1,10 +1,33 @@
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"];
 
 const carrito = [
-    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
-    { nombre: 'Televisión', precio: 100 },
-    { nombre: 'Tablet', precio: 200 },
-    { nombre: 'Audifonos', precio: 300 },
-    { nombre: 'Teclado', precio: 400 },
-    { nombre: 'Celular', precio: 700 },
-]
+  { nombre: "Monitor 27 Pulgadas", precio: 500 },
+  { nombre: "Televisión", precio: 100 },
+  { nombre: "Tablet", precio: 200 },
+  { nombre: "Audifonos", precio: 300 },
+  { nombre: "Teclado", precio: 400 },
+  { nombre: "Celular", precio: 700 },
+];
+
+// Comprobar si un valor existe en un arreglo
+
+meses.forEach((mes) => {
+  if (mes === "Enero") {
+    console.log("Enero si existe");
+  }
+});
+
+// Includes solo funciona para arreglos que sólo tienen un índice
+const resultado = meses.includes("Diciembre");
+console.log(resultado);
+
+// En un arreglo de objetos se utiliza some
+
+const existe = carrito.some((producto) => producto.nombre === "Monitor Curvo");
+
+console.log(existe);
+
+// En un arreglo tradicional con some
+
+const existe2 = meses.some((mes) => mes === "Febrero");
+console.log(existe2);
