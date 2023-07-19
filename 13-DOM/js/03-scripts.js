997764aa7f68dbf1cfa4c9b8cfc3952ca678f684
Leonardo <leonardo38361@gmail.com>
2023-07-18 22:00:08 -0300

    getElementById

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,6 @@
+// retorna un resultado a diferencia de className
+const formulario = document.getElementById("formulario");
+console.log(formulario);
+
+const noExiste = document.getElementById("no-existe");
+console.log(noExiste);
