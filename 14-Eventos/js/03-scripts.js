b28ff3823268829ea45df8de683ddb95f04d4d4f
Leonardo <leonardo38361@gmail.com>
2023-07-19 21:32:08 -0300

    eventos sobre los inputs

---
+++ 14-Eventos/js/03-scripts.js
@@ -0,0 +1,31 @@
+const busqueda = document.querySelector(".busqueda");
+
+// busqueda.addEventListener("keydown", () => {
+//   console.log("escribiendo...");
+// });
+
+// busqueda.addEventListener("keyup", () => {
+//   console.log("escribiendo...");
+// });
+
+// busqueda.addEventListener("blur", () => {
+//   console.log("escribiendo...");
+// });
+
+// busqueda.addEventListener("copy", () => {
+//   console.log("escribiendo...");
+// });
+
+// busqueda.addEventListener("paste", () => {
+//   console.log("escribiendo...");
+// });
+
+// busqueda.addEventListener("cut", () => {
+//   console.log("escribiendo...");
+// });
+
+busqueda.addEventListener("input", (e) => {
+  if (e.target.value === "") {
+    console.log("Falló la validación");
+  }
+});
