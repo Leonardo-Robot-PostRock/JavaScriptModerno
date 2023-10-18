ed98a9d059f6de7a62e9bee1f9a97eea6481e7ab
Leonardo <leonardo38361@gmail.com>
2023-10-18 04:43:20 -0300

    agregacion de comentarios

--- 14-Eventos/js/06-scripts.js
+++ 14-Eventos/js/06-scripts.js
@@ -1,19 +1,21 @@
 // Event Bubbling
-const cardDiv = document.querySelector(".card");
-const infoDiv = document.querySelector(".info");
-const titulo = document.querySelector(".titulo");
+// El evento se prograpa hacia las clases padres
+//con e.stopPropagation evitamos eso
+const cardDiv = document.querySelector('.card');
+const infoDiv = document.querySelector('.info');
+const titulo = document.querySelector('.titulo');
 
-cardDiv.addEventListener("click", (e) => {
-  e.stopPropagation();
-  console.log("click en card");
+cardDiv.addEventListener('click', (e) => {
+	e.stopPropagation();
+	console.log('click en card');
 });
 
-infoDiv.addEventListener("click", (e) => {
-  e.stopPropagation();
-  console.log("click en info");
+infoDiv.addEventListener('click', (e) => {
+	e.stopPropagation();
+	console.log('click en info');
 });
 
-titulo.addEventListener("click", (e) => {
-  e.stopPropagation();
-  console.log("click en titulo");
+titulo.addEventListener('click', (e) => {
+	e.stopPropagation();
+	console.log('click en titulo');
 });
