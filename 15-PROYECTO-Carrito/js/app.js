24ee9fbb80daa618a775154264613c06c2a45d3f
Leonardo <leonardo38361@gmail.com>
2023-10-18 23:42:42 -0300

    accediendo a elementos de un producto para agregarlos al carrito

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -13,7 +13,25 @@ function cargarEvenListener() {
 //Funciones
 function agregarCurso(e) {
 	e.preventDefault();
+	const cursoSeleccionado = e.target.parentElement.parentElement;
 	if (e.target.classList.contains('agregar-carrito')) {
-		console.log(e.target);
+		leerDatosCursos(cursoSeleccionado);
 	}
+}
+
+//Leer el contenido del HTML al que le dimos click y extrae la información del curso
+function leerDatosCursos(curso) {
+	// console.log(curso);
+
+	//Crear un objeto con el contenido del curso actual
+	const infoCurso = {
+		imagen: curso.querySelector('img').src,
+		titulo: curso.querySelector('h4').textContent,
+		nombre: curso.querySelector('p').textContent,
+		precio: curso.querySelector('.precio > span').textContent,
+		id: curso.querySelector('a').getAttribute('data-id'),
+		cantidad: 1,
+	};
+
+	console.log(infoCurso);
 }
